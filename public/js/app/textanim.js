903aa28d84b8b57e7e605b46b8b5dd309d464f55
emilmork <emil.a.mork@gmail.com>
2013-10-26 23:59:21 +0200

    changed naming conventions on lists and text to sentence and sentenceList

--- public/js/app/textanim.js
+++ public/js/app/textanim.js
@@ -6,16 +6,17 @@ define([
     , function(_, $, Bacon) {
         //Initialize values and help functions
         var text = $("#title-message")
-            , list = ["Vi er tverrfaglige.","Vi drar på konferanser.","Vi skaper løsninger i team.","Vi har tre fagdager i året."]
-            , switchText = function(value){
+            , delay = 10000
+            , sentenceList = ["Vi er tverrfaglige.","Vi drar på konferanser.","Vi skaper løsninger i team.","Vi har tre fagdager i året."]
+            , switchSentence = function(value){
               text.fadeOut(500,function(){
                 text.html(value).fadeIn(500);
             });
         }
 
-        var arrayProp = Bacon.repeatedly(10000,list)
-            .toProperty(list[list.length-1])
+        var sentence = Bacon.repeatedly(delay,sentenceList)
+            .toProperty(sentenceList[sentenceList.length-1])
             .onValue(function(text){
-                switchText(text);   
+                switchSentence(text);   
             });
 });
