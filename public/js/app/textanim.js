b2f1a6e2f27f04de36449fe931b40f3ab9282366
emilmork <emil.a.mork@gmail.com>
2013-10-26 11:13:16 +0200

    changed text animation to frp (semi)

--- public/js/app/textanim.js
+++ public/js/app/textanim.js
@@ -4,22 +4,18 @@ define([
     , "bacon"
     ]
     , function(_, $, Bacon) {
+        //Initialize values and help functions
+        var text = $("#title-message")
+            , list = ["Vi er tverrfaglige.","Vi drar på konferanser.","Vi skaper løsninger i team.","Vi har tre fagdager i året."]
+            , switchText = function(value){
+              text.fadeOut(500,function(){
+                text.html(value).fadeIn(500);
+            });
+        }
 
-
-        var list = new Array("Vi er tverrfaglige","Vi drar på konferanser","Vi skaper løsninger i team");
-
-        var count = 0;
-
-        var text = $("#title-message").html(list[0]);
-
-        setInterval(function(){
-        	 text.fadeOut(500, function() {
-        	 	count++;
-        	 	if(count == list.length)count=0;
-    			text.html(list[count]).fadeIn(500);
-  			});
-       		
-        },10000);
-        
-
+        var arrayProp = Bacon.repeatedly(10000,list)
+            .toProperty(list[list.length-1])
+            .onValue(function(text){
+                switchText(text);   
+            });
 });
