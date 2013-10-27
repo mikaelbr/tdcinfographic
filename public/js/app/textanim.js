54ac4d08389b2d84079614c2bd806c39f7ee08b9
Mikael Brevik <mikaelbre@gmail.com>
2013-10-27 11:22:04 +0100

    Adds some clean-up and functionalizing text animations

--- public/js/app/textanim.js
+++ public/js/app/textanim.js
@@ -5,18 +5,23 @@ define([
     ]
     , function(_, $, Bacon) {
         //Initialize values and help functions
-        var text = $("#title-message")
-            , delay = 10000
-            , sentenceList = ["Vi er tverrfaglige.","Vi drar på konferanser.","Vi skaper løsninger i team.","Vi har tre fagdager i året."]
-            , switchSentence = function(value){
-              text.fadeOut(500,function(){
-                text.html(value).fadeIn(500);
-            });
-        }
+        var $text = $("#title-message")
+          , delay = 10000
+          , sentenceList = [
+            "Vi er tverrfaglige.",
+            "Vi drar på konferanser.",
+            "Vi skaper løsninger i team.",
+            "Vi har tre fagdager i året."
+          ]
+          , switchSentence = function($el){
+            return function (value) {
+              $el.fadeOut(500,function(){
+                $el.html(value).fadeIn(500);
+              });
+            };
+          };
 
-        var sentence = Bacon.repeatedly(delay,sentenceList)
-            .toProperty(sentenceList[sentenceList.length-1])
-            .onValue(function(text){
-                switchSentence(text);   
-            });
+        var sentence = Bacon.repeatedly(delay, sentenceList)
+                            .toProperty(_.last(sentenceList))
+                            .onValue(switchSentence($text));
 });
