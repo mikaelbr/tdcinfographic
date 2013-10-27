5b0658cba91d7704bcbb9f5fda624159a9dcfb20
Mikael Brevik <mikaelbre@gmail.com>
2013-10-27 10:54:55 +0100

    Adds weight to the button, with validation and states.

--- public/js/app/submit.js
+++ public/js/app/submit.js
@@ -6,12 +6,46 @@ require([
     ]
     , function(io, _, $, Bacon) {
 
-    var socket = io.connect();
-    $(".button")
-        .asEventStream('click')
-        .doAction(".preventDefault")
-        .debounceImmediate(1000)
+    var socket = io.connect()
+      , $buttonWrapper = $("#button-wrapper")
+      , $maxWeightNotice = $(".max-weight-notice")
+      , $thanksNotice = $(".thanks-notice")
+      , limit = 300
+      , isOverLimit = function (weight) {
+        return weight >= limit;
+    };
+
+    var submit = $(".button")
+                        .asEventStream('click')
+                        .doAction(".preventDefault")
+                        .filter(function (e) {
+                            var $t = $(e.currentTarget);
+                            return !$t.parent().hasClass("disabled");
+                        });
+
+    submit.debounceImmediate(1000)
         .onValue(function () {
             socket.emit('sample');
         });
+
+    // Show current weight!
+    var weight = Bacon.fromEventTarget(socket, 'grams')
+                    .filter(_.isNumber)
+                    .map(Number);
+
+    weight.assign($("#number-individual"), "text");
+
+    // Activate/Deactivate button, based on validity.
+    var valid = weight.map(isOverLimit);
+    valid.map(function (isValid) {
+            return isValid ? "disabled" : "enabled";
+        })
+        .assign($buttonWrapper, "attr", "class");
+    valid.assign($maxWeightNotice, "toggle");
+
+
+    // Show/Hide thank you message
+    var thanks = submit.toProperty(false).map(Boolean)
+    thanks.assign($thanksNotice, "toggle");
+    submit.map(Boolean).delay(3000).not().log().assign($thanksNotice, "toggle")
 });
